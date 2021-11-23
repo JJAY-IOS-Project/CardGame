00505b4b6ed7d0fd2edaa322dc530b992bf3df1b
sangy1 <56744256+sangy1@users.noreply.github.com>
2021-11-23 18:19:04 -0500

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by SangWon Park on 11/23/21.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
